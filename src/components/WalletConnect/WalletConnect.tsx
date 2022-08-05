d0fb5fc334e44fbfbb6f76383262ab0fe2f7e719
MIYABETaisei <mytspe46@gmail.com>
2022-08-05 21:14:24 +0900

    ⚡️ Handle wrong network

--- src/components/WalletConnect/WalletConnect.tsx
+++ src/components/WalletConnect/WalletConnect.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@mantine/core";
-import { useEthers } from "@usedapp/core";
+import { Button, Group, Text } from "@mantine/core";
+import { Goerli, useEthers } from "@usedapp/core";
 
 export const WalletConnect = () => {
-  const { activateBrowserWallet, account, deactivate } = useEthers();
+  const { activateBrowserWallet, account, deactivate, chainId, switchNetwork } =
+    useEthers();
   if (account) {
-    return <Button onClick={deactivate}>Disconnect</Button>;
+    if (chainId === Goerli.chainId) {
+      return <Button onClick={deactivate}>Disconnect</Button>;
+    } else {
+      return (
+        <Group position="right">
+          <Text color="red">Wrong network</Text>
+          <Button onClick={() => switchNetwork(Goerli.chainId)}>
+            Swich network
+          </Button>
+        </Group>
+      );
+    }
   } else {
     return <Button onClick={activateBrowserWallet}>connect to Metamask</Button>;
   }
